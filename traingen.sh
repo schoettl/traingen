b88c2dee08eccc87cae09e1ed4a30602e4b091b9
Jakob Schöttl <jschoett@gmail.com>
2016-08-29 21:48:32 +0200

    Update wrapper script

--- traingen.sh
+++ traingen.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Wrapper script for Traingen (Java program, class TraingenApp)
 
+# To compile the jar with dependencies:
+# mvn clean compile assembly:single
+
 readonly PROGDIR=$(dirname "$(readlink -m "$0")")
 
 readonly TRAINGEN_VERSION=0.0.1-SNAPSHOT
 
 main() {
-    # Classpath according to maven defaults
-    #java -classpath "$PROGDIR"/traingen/target/classes/:"$PROGDIR"/../Software/VadereUtils/target/classes/:"$PROGDIR"/../Software/VadereState/target/classes/ edu.hm.cs.vadere.seating.traingen.TraingenApp "$@"
-    java -jar "$PROGDIR"/traingen/target/traingen-$TRAINGEN_VERSION.jar "$@"
+    java -jar "$PROGDIR"/traingen/target/traingen-${TRAINGEN_VERSION}-jar-with-dependencies.jar "$@"
 }
 
 main "$@"
