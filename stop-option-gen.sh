b473faa9c60ae92c6b746ef9183ab8b47a51e595
Jakob Schöttl <jschoett@gmail.com>
2016-10-08 20:01:04 +0200

    Add option for trailing backslash

--- stop-option-gen.sh
+++ stop-option-gen.sh
@@ -7,22 +7,34 @@ readonly PROGNAME=$(basename "$0")
 
 printUsage() {
     cat <<EOF
-usage: $PROGNAME < time-and-enter.txt
+usage: $PROGNAME [ -b ] < time-and-enter.txt
+  -b   append a backslash after each --stop option
 EOF
 }
 
 main() {
-    if [[ $# != 0 ]]; then
+    if (( $# > 1 )); then
         printUsage
         exit 1
+    fi
+    declare appendBackslash=0
+    if (( $# == 1 )); then
+        case $1 in
+            -b)
+                appendBackslash=1
+                ;;
+            *)
+                printUsage
+                exit 1
+                ;;
+        esac
     fi
 
     cat \
     | awk '{ split($1, a, ":"); s = a[1]*3600+a[2]*60; print s, $2 }' \
     | awk 'NR==1{ t=$1 }; { print ($1-t), $2 }' \
     | awk '{ printf "--stop=%s,top,%d\n", $1, $2 }' \
-    | awk -v ab=1 '{ if(ab) print $0 " \\"; else print }'
-    # ab = append backslash
+    | awk -v ab="$appendBackslash" '{ if(ab) print $0 " \\"; else print }'
 }
 
 main "$@"
